b06c0463007d9e5a37f60e0c914ceff8e7fa1ee8
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-03-03 19:48:17 +0530

    reverted to original  changes

--- stats.c
+++ stats.c
@@ -1,5 +1,4 @@
 #include "stats.h"
-#include "math.h"
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
@@ -9,28 +8,7 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     static int totalValue = 0;
     s.average = 0;
     s.min = 0;
-    s.max = 0;
-    
-   memcpy(&data[0],numberset,setlength);
-    
-   for( int  i =0 ;i <setlength;i++)
-    {
-       /* total sum computation */
-       totalValue += data[i];
-       /* max value computation*/
-       if(maxData< data[i])
-       {
-           maxData = data[i];
-       }
-       /* min value computation*/
-       if(minData> data[i])
-       {
-           minData = data[i];
-       }   
-    }
-    s.average = totalValue/setlength;
-    s.min = minData;
-    s.max = maxData;    
+    s.max = 0;   
 }
 
 int emailAlertCallCount = 0;
