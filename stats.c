0d00568c11bcf43e0f13965dc9251ee9b5456dec
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-03-03 20:19:43 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,4 +1,6 @@
 #include "stats.h"
+int emailAlertCallCount = 0;
+int ledAlertCallCount = 0;
 
 Stats compute_statistics(const float* numberset, int setlength) {
     Stats s;
@@ -10,8 +12,39 @@ Stats compute_statistics(const float* numberset, int setlength) {
     s.min = 0;
     s.max = 0;
     
+    memcpy(&data[0],numberset,setlength);
+    
+    for( int  i =0 ;i <setlength;i++)
+    {
+       /* total sum computation */
+       totalValue += data[i];
+       /* max value computation*/
+       if(maxData< data[i])
+       {
+           maxData = data[i];
+       }
+       /* min value computation*/
+       if(minData> data[i])
+       {
+           minData = data[i];
+       }   
+    }
+    s.average = totalValue/setlength;
+    s.min = minData;
+    s.max = maxData;  
+    
     return s;
 }
 
-int emailAlertCallCount = 0;
-int ledAlertCallCount = 0;
+void emailAlerter()
+{
+    emailAlertCallCount = 1;
+}
+
+
+void ledAlerter()
+{
+    ledAlertCallCount = 1;
+}
+
+
