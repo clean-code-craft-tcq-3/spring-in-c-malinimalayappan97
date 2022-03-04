033dbae06fa6f9384109e90d13dd74a8aba62257
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-03-04 21:01:46 +0530

    Update Stats1.c

--- Stats1.c
+++ Stats1.c
@@ -1,1 +1,17 @@
+#include "Stats1.h"
 
+int emailAlertCallCount = 0;
+int ledAlertCallCount = 0;
+
+
+int emailAlerter()
+{
+    emailAlertCallCount = 1;
+    return emailAlertCallCount;
+}
+
+int ledAlerter()
+{
+    ledAlertCallCount = 1;
+    return ledAlertCallCount;
+}
