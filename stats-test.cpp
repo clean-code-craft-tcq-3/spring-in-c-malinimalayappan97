13d06c97072b7a3665b3437ce41b95143a973691
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-03-04 21:24:56 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -35,7 +35,7 @@ TEST_CASE("raises alerts when max is greater than threshold") {
     int alerter_funcptr alerters[] = {emailAlerter, ledAlerter};
 
     float numberset[] = {99.8, 34.2, 4.5};
-    int setlength = sizeof(numberset) / sizeof(numberset[0]);
+    setlength = sizeof(numberset) / sizeof(numberset[0]);
     Stats computedStats = compute_statistics(numberset, setlength);
 
     const float maxThreshold = 10.2;
